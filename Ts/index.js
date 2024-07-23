9afb3f5a6a525e08ce18b8cae7f28ca50ccc648c
HasanCoder66 <hasanashraf874@gmail.com>
2024-07-23 17:57:28 +0500

    3rd video done with practice

--- Ts/index.js
+++ Ts/index.js
@@ -1,7 +1,11 @@
-var number = 5;
-number = 55;
-console.log(number);
-console.log('hello world');
-// in ts we type this array 
-var multipleValues = ['hasan', 21];
+var num = 5;
+num = 55;
+console.log(num);
+console.log("hello world");
+// in ts we type this array
+var multipleValues = ["hasan", 21];
 console.log(multipleValues);
+var sum = function (a, b) {
+    return a + b;
+};
+console.log(sum(10, 5));
--- Ts/index.ts
+++ Ts/index.ts
@@ -1,8 +1,14 @@
-let number: number = 5
-number = 55
-console.log(number);
-console.log('hello world');
+let num: number = 5;
+num = 55;
+console.log(num);
+console.log("hello world");
 
-// in ts we type this array 
-const multipleValues: unknown[] = ['hasan', 21]
-console.log(multipleValues)
+// in ts we type this array
+const multipleValues: unknown[] = ["hasan", 21];
+console.log(multipleValues);
+
+const sum = (a: number, b: number): number => {
+  return a + b
+};
+
+console.log(sum(10, 5));
