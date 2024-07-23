d5ac44da22dd21f7d09f27ab551ef60678513486
HasanCoder66 <hasanashraf874@gmail.com>
2024-07-23 16:48:56 +0500

    2nd video done

--- Ts/index.js
+++ Ts/index.js
@@ -1,7 +1,7 @@
 var number = 5;
 number = 55;
 console.log(number);
-console.log("hello world");
-// in ts we type this array
-var multipleValues = ["hasan", 21];
+console.log('hello world');
+// in ts we type this array 
+var multipleValues = ['hasan', 21];
 console.log(multipleValues);
