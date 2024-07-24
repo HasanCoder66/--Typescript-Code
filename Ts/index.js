340595363ef0f09ddf54ad57fff28ff5d22aa6a0
HasanCoder66 <hasanashraf874@gmail.com>
2024-07-24 17:01:12 +0500

    video 4 done

--- Ts/index.js
+++ Ts/index.js
@@ -1,11 +1,45 @@
 var num = 5;
 num = 55;
-console.log(num);
-console.log("hello world");
+// console.log(num);
+// console.log("hello world");
 // in ts we type this array
 var multipleValues = ["hasan", 21];
-console.log(multipleValues);
+// console.log(multipleValues);
 var sum = function (a, b) {
     return a + b;
 };
-console.log(sum(10, 5));
+// console.log(sum(10, 5));
+// video no 4 Practices ===>
+var computedVal = Math.sqrt(16);
+// console.log(computedVal)
+var myFullName = "Muhammad Hasan";
+var myFirstName = "Muhammad";
+var myLastName = "Hassan";
+// console.log(myFirstName + " " + myLastName)
+// check length of our sentence
+var sentence = "please subscribe channel";
+var sentenceLength = sentence.length;
+// console.log(sentenceLength);
+var text = sentence;
+// text ko uppercase ker rahay hain yaha
+var toUpperCase = text.toUpperCase();
+// console.log(toUpperCase);
+// text ko lowercase ker rahay hain yaha
+var toLowerCase = text.toLowerCase();
+// console.log(toLowerCase);
+// Question 1 from thapa ?
+// convert longText into short text using substring() method 
+var longText = "Hi, how are you! i am doing well' what are you doing today ? by the way nothing! just today i m gonna chill😋";
+var shortText = longText.substring(0, 10);
+// console.log(shortText);
+// Question 2 from thapa
+// check equal values answer must be in boolean
+var checkEqual = (myFirstName === myFirstName);
+// console.log(checkEqual); //  return true
+var checkEqual2 = (myFirstName === myLastName);
+// console.log(checkEqual); //  return false
+// Question 3 from thapa 
+var product = 'Pizza';
+var price = 650;
+var finalResult = "this large ".concat(product, " cost is Rs.").concat(price);
+console.log(finalResult);
--- Ts/index.ts
+++ Ts/index.ts
@@ -1,14 +1,66 @@
 let num: number = 5;
 num = 55;
-console.log(num);
-console.log("hello world");
+// console.log(num);
+// console.log("hello world");
 
 // in ts we type this array
 const multipleValues: unknown[] = ["hasan", 21];
-console.log(multipleValues);
+// console.log(multipleValues);
 
 const sum = (a: number, b: number): number => {
-  return a + b
+  return a + b;
 };
 
-console.log(sum(10, 5));
+// console.log(sum(10, 5));
+
+// video no 4 Practices ===>
+
+let computedVal: number = Math.sqrt(16);
+// console.log(computedVal)
+
+let myFullName: String = "Muhammad Hasan";
+let myFirstName: string = "Muhammad";
+let myLastName: string = "Hassan";
+
+// console.log(myFirstName + " " + myLastName)
+
+// check length of our sentence
+let sentence: string = "please subscribe channel";
+let sentenceLength: number = sentence.length;
+
+// console.log(sentenceLength);
+
+let text: string = sentence;
+
+// text ko uppercase ker rahay hain yaha
+let toUpperCase: string = text.toUpperCase();
+// console.log(toUpperCase);
+
+// text ko lowercase ker rahay hain yaha
+let toLowerCase: string = text.toLowerCase();
+// console.log(toLowerCase);
+
+
+// Question 1 from thapa ?
+// convert longText into short text using substring() method 
+let longText: string = "Hi, how are you! i am doing well' what are you doing today ? by the way nothing! just today i m gonna chill😋"
+
+let shortText : string = longText.substring(0,10)
+// console.log(shortText);
+
+
+// Question 2 from thapa
+// check equal values answer must be in boolean
+const checkEqual : boolean =( myFirstName === myFirstName )
+// console.log(checkEqual); //  return true
+
+const checkEqual2 : boolean =( myFirstName === myLastName )
+// console.log(checkEqual); //  return false
+
+
+// Question 3 from thapa 
+let product: string = 'Pizza';
+let price: number = 650;
+
+let finalResult = `this large ${product} cost is Rs.${price}`
+// console.log(finalResult);
